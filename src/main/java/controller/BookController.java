b7c30008ef60011f8ac0769269dba85ef372f06f
Thong Nguyen <thongnguyen@pixelcent.com>
2023-01-08 17:47:43 +0700

    Book + Book Lanugage + Render book GUI

---
+++ src/main/java/controller/BookController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import dao.BookDAO;
+import entity.Book;
+import enumeration.AuthorStatus;
+import java.util.*;
+
+public class BookController {
+
+    public List<Book> getAll() {
+        BookDAO bd = new BookDAO();
+        return bd.getAll();
+    }
+    
+    public void create(Book book){
+        BookDAO bd = new BookDAO();
+        bd.create(book);
+    }
+
+    public boolean delete(Long id) {
+        BookDAO ad = new BookDAO();
+        BookDAO bd = new BookDAO();
+        int count = bd.countByColumn("author_id", Long.toString(id));
+        if(count > 0) return false;
+        
+        ad.delete(id);
+        return true;
+    }
+    
+    public List<Long> deleteMany(List<Long> list) {
+        List<Long> fail = new ArrayList();
+        for (Long id : list) {
+            if(!delete(id)) fail.add(id);
+        }
+        return fail;
+    }
+}
